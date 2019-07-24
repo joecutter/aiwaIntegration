1d76350afde42fa2518caa217e6992327e183b87
joecutter <cjnjenga@gmail.com>
2019-07-24 21:08:12 +0300

    clean

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,10 @@
-const Web3 = require('aion-web3')
-const web3 = new Web3(
-    new Web3.providers.HttpProvider("https://aion.api.nodesmith.io/v1/mastery/jsonrpc?apiKey=ae8d2a52c4994dd7bbbc06ac62b398d2")
-    );
+const Web3 = require('aion-web3');
 const $ = require("jquery");
 
 window.onload = async () => {
     $('#grantAccess').on('click', async () => {
         try {
-          const accountAddress = await window.aiwa.enable();  // grants access. Best practice to do it in a button. 
-          console.log("wallets ", accountAddress)
+          const accountAddress = await window.aiwa.enable();  // grants access. Best practice to do it in a button.
           document.getElementById("grantsAccess").innerHTML = 'account address: ' + JSON.stringify(accountAddress);
         } catch(err){
           document.getElementById("grantsAccess").innerHTML = err.message;
@@ -17,9 +13,9 @@ window.onload = async () => {
 
     $('#fetchAddress').on('click', async () => {
         try {
-          const accountAddress = await window.aionweb3.eth.accounts;  // grants access. Best practice to do it in a button. 
-          console.log("wallets ", accountAddress)
-          document.getElementById("addressAccess").innerHTML = 'account address: ' + JSON.stringify(accountAddress);
+          const address = await window.aionweb3.eth.accounts; 
+          console.log("wallets ", address)
+          document.getElementById("addressAccess").innerHTML = 'account address: ' + JSON.stringify(address);
         } catch(err){
           document.getElementById("addressAccess").innerHTML = err.message;
         }
